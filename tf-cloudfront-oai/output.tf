b6a75ec24837b69ea758febfc46ef150e191f317
Phil Crocker <23005538+philipcrocker@users.noreply.github.com>
2022-10-04 02:39:02 -0600

    s3 restore point

--- tf-cloudfront-oai/output.tf
+++
@@ -1,7 +0,0 @@
-output "cloudfront_distribution_domain_name" {
-  value = module.cloudfront_with_oai.cloudfront_distribution_domain_name
-}
-
-output "s3_bucket_bucket_domain_name" {
-  value = module.origin_bucket.s3_bucket_bucket_domain_name
-}
